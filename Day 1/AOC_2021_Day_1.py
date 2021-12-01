5ad5c8530b3c33b1c02d0abda895b5427ca73612
Gaziz Zhotabayev <zhotag01@kits.kfplc.com>
2021-12-01 11:42:51 +0000

    day 1 puzzles

---
+++ Day 1/AOC_2021_Day_1.py
@@ -0,0 +1,18 @@
+import csv, os, sys
+
+file_name = 'input_day_1.txt'
+with open(os.path.join(sys.path[0], file_name)) as f:
+    #parse the .txt into a list of integers
+    lst1 = [int(i) for i in f.read().split('\n') if i != ''] 
+
+#f will count the # of times there was an increase in consecutive numbers of a list
+f = lambda lst: sum(1 if lst[i+1] > e else 0 for i, e in enumerate(lst[:-1]))
+
+ans1 = f(lst1)
+print(f'answer to first puzzle of day 1 is: {ans1}')
+
+#transform the list to a rolling 3 window
+lst2 = [sum(lst1[i:i+3]) for i, e in enumerate(lst1[:-2])]
+
+ans2 = f(lst2)
+print(f'answer to second puzzle of day 1 is: {ans2}')
