4c2a1650d2d87eaf5733d7ff361fb254144926d8
Gaziz Zhotabayev <zhotag01@kits.kfplc.com>
2021-12-03 21:42:45 +0000

    puzzle 10 2020

--- 2020/Day 10/AOC_2020_Day_10.py
+++ 2020/Day 10/AOC_2020_Day_10.py
@@ -1,13 +1,15 @@
 import os, sys
-from functools import reduce
 
 file_name = 'input_day_10.txt'
+#file_name = 'test_10.txt'
 day = file_name.split('.')[0].split('_')[-1]
 
 with open(os.path.join(sys.path[0], file_name)) as f:
     lst = [int(line) for line in f.read().split('\n') if line != ''] 
-    x = []
-    for n in lst: x += list(range(n-3, n))
-    lst += x + [0, max(lst) + 3]
+    lst += [0, max(lst) + 3]
 
-    print(lst[:10])
+    lst = [i for i in sorted(set(lst)) if i >= 0]
+    gaps = [n-lst[i] for i, n in enumerate(lst[1:])]
+
+    ans1 = gaps.count(3) * gaps.count(1)
+    print(f'answer to first puzzle of day {day} is: {ans1}')
